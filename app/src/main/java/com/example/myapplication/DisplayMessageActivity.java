f79721105faa53c9da15b9c09fa11961fc06fa47
satvik20 <satvikshukla10122000@gmail.com>
2020-11-25 23:31:09 +0530

    tab layout included

---
+++ app/src/main/java/com/example/myapplication/DisplayMessageActivity.java
@@ -0,0 +1,152 @@
+package com.example.myapplication;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.apache.sshd.client.SshClient;
+import org.apache.sshd.client.channel.ClientChannel;
+import org.apache.sshd.client.channel.ClientChannelEvent;
+import org.apache.sshd.client.session.ClientSession;
+import org.apache.sshd.common.channel.Channel;
+import org.apache.sshd.server.forward.AcceptAllForwardingFilter;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.EnumSet;
+import java.util.concurrent.TimeUnit;
+
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
+import android.text.method.ScrollingMovementMethod;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+    ClientChannel channel;
+    EditText shellInput;
+    TextView shellOutput;
+    String host,port,username,password;
+    String command, output;
+    Boolean isActive;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+
+        //set I/O
+        shellInput = (EditText) findViewById(R.id.shellText);
+
+        shellOutput = findViewById(R.id.textView);
+        shellOutput.setMovementMethod(new ScrollingMovementMethod());
+
+        //Get user credentials from indent
+        Intent intent = getIntent();
+        host = intent.getStringExtra("host");
+        port = intent.getStringExtra("port");
+        username = intent.getStringExtra("username");
+        password = intent.getStringExtra("password");
+        isActive = TRUE;
+        command ="pwd\n";
+
+        //Logging
+        System.out.println("host:"+host+"\nusername:"+username);
+        output = "username:"+username+"\n"+"password:"+password;
+        shellOutput.setText(output);
+
+        float v=0;
+        shellOutput.setTranslationX(800);
+        shellOutput.setAlpha(v);
+
+        shellOutput.animate().translationX(0).alpha(1).setDuration(800).setStartDelay(100).start();
+
+
+        //Setting user.com property manually since isn't set by default
+        String key = "user.home";
+        Context Syscontext;
+        Syscontext = getApplicationContext();
+        String val = Syscontext.getApplicationInfo().dataDir;
+        System.setProperty(key, val);
+
+        //SSH
+        SshClient client = SshClient.setUpDefaultClient();
+        client.setForwardingFilter(AcceptAllForwardingFilter.INSTANCE);
+        client.start();
+
+        //Starting new thread because network processes can interfere with UI if started in main thread
+        Thread thread = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                try  {
+                    //Connection establishment and authentication
+                    try (ClientSession session = client.connect("abigit03", "tty.sdf.org", 22).verify(10000).getSession()) {
+                        session.addPasswordIdentity("OFhjwdWgsZLvw");
+                        session.auth().verify(50000);
+                        System.out.println("Connection establihed");
+
+                        startShell(session);
+
+                        ByteArrayOutputStream responseStream = new ByteArrayOutputStream();
+                        channel.setOut(responseStream);
+                        while (true) {
+                            if ("exit".equals(command)) {
+                                channel.close(false);
+                                break;
+                            }
+                            else if (!isActive){
+                                Thread.sleep(2000);
+                            }
+                            else {
+                                channel.open().verify(5, TimeUnit.SECONDS);
+                                try (OutputStream pipedIn = channel.getInvertedIn()) {
+                                    pipedIn.write(command.getBytes());
+                                    pipedIn.flush();
+                                }
+
+                                channel.waitFor(EnumSet.of(ClientChannelEvent.CLOSED),
+                                        TimeUnit.SECONDS.toMillis(5));
+                                String responseString = new String(responseStream.toByteArray());
+                                System.out.println(responseString);
+                                output(responseString);
+
+                                //set shell-flag to inactive
+                                isActive = FALSE;
+                            }
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    finally {
+                        client.stop();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        thread.start();
+
+
+    }
+
+    public void startShell(ClientSession session) throws IOException {
+        this.channel = session.createChannel(Channel.CHANNEL_SHELL);
+        System.out.println("Starting shell");
+    }
+
+    public void execute(View view) throws IOException {
+        //update command variable
+        isActive = TRUE;
+        command += shellInput.getText().toString() + "\n";
+    }
+
+    public void output(String out) throws IOException {
+        shellOutput.setText(out);
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/first.java
@@ -0,0 +1,120 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class first extends AppCompatActivity {
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        SharedPreferences p=getSharedPreferences("PREFERENCES", MODE_PRIVATE);
+        String s=p.getString("Dark","");
+        if (s.equals("yes")) {
+            setTheme(R.style.DarkTheme);
+        } else {
+            setTheme(R.style.LightTheme);
+        }
+        setContentView(R.layout.firstact);
+        getSupportActionBar().setTitle("NeTTweaak");
+        System.out.println("Testinggggggggggggggggggggg");
+        tabLayout=findViewById(R.id.tablayout);
+        viewPager=findViewById(R.id.Viewpager);
+
+        tabLayout.addTab(tabLayout.newTab().setText("Connection"));
+        tabLayout.addTab((tabLayout.newTab().setText("Log")));
+        tabLayout.addTab(tabLayout.newTab().setText("Extra"));
+        tabLayout.addTab((tabLayout.newTab().setText("Setting")));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+        final madapter adapter=new madapter(getSupportFragmentManager(),this,tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(onTabSelectedListener(viewPager));
+
+
+    }
+    private TabLayout.OnTabSelectedListener onTabSelectedListener(final ViewPager pager) {
+        return new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                pager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        };
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.action_menu,menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()){
+            case R.id.home:
+                Intent intent=new Intent(this,first.class);
+                startActivity(intent);
+                break;
+            case R.id.ctheme:
+                Intent i=new Intent(this,theme.class);
+                startActivity(i);
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    public void sendMessage(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        EditText portField = (EditText) findViewById(R.id.portField);
+        EditText usernameField = (EditText) findViewById(R.id.usernameField);
+        EditText passwordField = (EditText) findViewById(R.id.passwordField);
+
+        String host = editText.getText().toString();
+        String port = portField.getText().toString();
+        String username = usernameField.getText().toString();
+        String password = passwordField.getText().toString();
+
+        intent.putExtra("host", host);
+        intent.putExtra("port", port);
+        intent.putExtra("username", username);
+        intent.putExtra("password", password);
+        startActivity(intent);
+        finish();
+        /*
+        Intent intent2 = new Intent(this, SSHservice.class);
+        startService(intent2);*/
+    }
+    public void stopMessage(View view) {
+        // Do something in response to button
+        Intent intent3 = new Intent(this, SSHservice.class);
+        stopService(intent3);
+        finish();
+    }
+
+}
