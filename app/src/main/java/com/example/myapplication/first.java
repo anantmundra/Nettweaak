56246b469e4c9a666a6fe63457dac39ab40d99bc
satvik20 <satvikshukla10122000@gmail.com>
2020-11-26 12:18:59 +0530

    notification added

--- app/src/main/java/com/example/myapplication/first.java
+++ app/src/main/java/com/example/myapplication/first.java
@@ -1,7 +1,12 @@
 package com.example.myapplication;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -9,10 +14,12 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.tabs.TabLayout;
@@ -94,6 +101,7 @@ public class first extends AppCompatActivity {
         EditText portField = (EditText) findViewById(R.id.portField);
         EditText usernameField = (EditText) findViewById(R.id.usernameField);
         EditText passwordField = (EditText) findViewById(R.id.passwordField);
+        Button button=(Button) findViewById(R.id.button);
 
         String host = editText.getText().toString();
         String port = portField.getText().toString();
@@ -104,8 +112,39 @@ public class first extends AppCompatActivity {
         intent.putExtra("port", port);
         intent.putExtra("username", username);
         intent.putExtra("password", password);
-        startActivity(intent);
-        finish();
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String msg="Click the notification to start shell";
+                Intent intent=new Intent(getApplicationContext(),DisplayMessageActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                intent.putExtra("message",msg);
+                PendingIntent pi=PendingIntent.getActivity(getApplicationContext(),0,intent,PendingIntent.FLAG_UPDATE_CURRENT);
+                NotificationCompat.Builder builder= new NotificationCompat.Builder(getApplicationContext(), "notify")
+                        .setSmallIcon(R.drawable.ic_baseline_add_alert_24)
+                        .setContentTitle("Start Shell")
+                        .setContentText(msg)
+                        .setAutoCancel(true);
+                builder.setContentIntent(pi);
+
+                NotificationManager nm=(NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+                {
+                    String channelId = "Your_channel_id";
+                    NotificationChannel channel = new NotificationChannel(
+                            channelId,
+                            "Channel human readable title",
+                            NotificationManager.IMPORTANCE_HIGH);
+                    nm.createNotificationChannel(channel);
+                    builder.setChannelId(channelId);
+                }
+                nm.notify(0,builder.build());
+                //   startActivity(intent);
+
+            }
+        });
+
         /*
         Intent intent2 = new Intent(this, SSHservice.class);
         startService(intent2);*/
